04049892a3676153133b2a2873a3cbe0151166a7
Charlie Bayne <baynec2@gmail.com>
2024-10-04 14:33:06 -0700

    fixed metadata

--- assemble_figures.sh
+++ assemble_figures.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Copying final figures to directory 
-cp 02_figures/**/*.png 07_final_manuscript/
+cp 02_figures/**/Figure*.pdf 07_final_manuscript/
 
 # Copying supplemental 
 ## Figures
- cp 03_supplemental/**/*.png 07_final_manuscript/supplemental/
+ cp 03_supplemental/**/S[[:digit:]].pdf 07_final_manuscript/supplemental/
 
 ## Tables
 cp 03_supplemental/tables/*.xlsx 07_final_manuscript/supplemental/
